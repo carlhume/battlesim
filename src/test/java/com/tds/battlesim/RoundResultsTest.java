99944295a57586221adb7a4db5825a9ba7d3b55b
Carl Hume <chume@360incentives.com>
2018-03-04 12:10:35 -0500

    Refactoring:  simplified the test logic by relying on default Troop types.

--- src/test/java/com/tds/battlesim/RoundResultsTest.java
+++ src/test/java/com/tds/battlesim/RoundResultsTest.java
@@ -8,16 +8,8 @@ public class RoundResultsTest {
 
 	@Test
 	public void testRoundResultsAreInitializedWithStartingTroopCounts() {
-		Troops troops = new Troops();
-		troops.setCount( 10 );
-		Side side = new Side();
-		side.addTroops( troops );
-		
-		Troops moreTroops = new Troops();
-		moreTroops.setCount( 5 );
-		Side anotherSide = new Side();
-		anotherSide.addTroops( moreTroops );
-		
+		Side side = new Side( "Side", 10 );
+		Side anotherSide = new Side( "Another Side", 5 );		
 		RoundResults results = new RoundResults( side, anotherSide );
 		assertEquals( "Results should be initialized with starting troops from first side", 10, results.getSideTroopCountAtStartOfRound() );
 		assertEquals( "Results should be initialized with starting troops from second side", 5, results.getAnotherSideTroopCountAtStartOfRound() );
